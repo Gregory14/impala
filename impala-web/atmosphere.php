41d056ec71ab841cd29cca2476b1ca1bf72f0b6e
mbouras <marine.bouras@gmail.com>
2014-06-23 22:58:49 +0200

    Mise en page CSS page La Clique (équipe)

--- impala-web/atmosphere.php
+++ impala-web/atmosphere.php
@@ -6,6 +6,51 @@ include("includes/header.php");
 ?>
         <h2>La clique d'Atmosphère</h2>
         <section id="team-content">
-            <p>Ici le contenu de la page</p>
+            <div class="team-category">
+                <h3>Stratégie, Réflexion utilisateur, Créas</h3>
+                <div class="inline-block">
+                    <img src="#" alt="Visuel membre" />
+                    <h4>Nom de la personne</h4>
+                    <p>Petit blabla sur la personne. C'est le plsus pro, le plus sérieux, bref le meilleur</p>
+                </div>
+                <div class="inline-block">
+                    <img src="#" alt="Visuel membre" />
+                    <h4>Nom de la personne</h4>
+                    <p>Petit blabla sur la personne. C'est le plsus pro, le plus sérieux, bref le meilleur</p>
+                </div>
+                <div class="inline-block">
+                    <img src="#" alt="Visuel membre" />
+                    <h4>Nom de la personne</h4>
+                    <p>Petit blabla sur la personne. C'est le plsus pro, le plus sérieux, bref le meilleur</p>
+                </div>
+                <div class="inline-block">
+                    <img src="#" alt="Visuel membre" />
+                    <h4>Nom de la personne</h4>
+                    <p>Petit blabla sur la personne. C'est le plsus pro, le plus sérieux, bref le meilleur</p>
+                </div>
+            </div>
+            <div class="team-category">
+                <h3>Les motions et les tech'</h3>
+                <div class="inline-block">
+                    <img src="#" alt="Visuel membre" />
+                    <h4>Nom de la personne</h4>
+                    <p>Petit blabla sur la personne. C'est le plsus pro, le plus sérieux, bref le meilleur</p>
+                </div>
+                <div class="inline-block">
+                    <img src="#" alt="Visuel membre" />
+                    <h4>Nom de la personne</h4>
+                    <p>Petit blabla sur la personne. C'est le plsus pro, le plus sérieux, bref le meilleur</p>
+                </div>
+                <div class="inline-block">
+                    <img src="#" alt="Visuel membre" />
+                    <h4>Nom de la personne</h4>
+                    <p>Petit blabla sur la personne. C'est le plsus pro, le plus sérieux, bref le meilleur</p>
+                </div>
+                <div class="inline-block">
+                    <img src="#" alt="Visuel membre" />
+                    <h4>Nom de la personne</h4>
+                    <p>Petit blabla sur la personne. C'est le plsus pro, le plus sérieux, bref le meilleur</p>
+                </div>
+            </div>
         </section>
 <?php include("includes/footer.php"); ?>
