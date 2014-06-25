ac8e97cf406d375f2773343bdc0a87c39ba53a5a
mbouras <marine.bouras@gmail.com>
2014-06-25 22:46:43 +0200

    Création page Validation commande

--- impala-web/includes/footer.php
+++ impala-web/includes/footer.php
@@ -3,7 +3,7 @@
             <div class="container">
             <div id="follow-us" class="inline-block">
                 <p class="inline-block">Suivez-nous sur</p>
-                <ul class="list-inline inline-block">
+                <ul class="social-networks list-inline inline-block">
                     <li><a class="facebook" href="#" title="Facebook" rel="external">Facebook</a></li>
                     <li><a class="twitter" href="#" title="Twitter" rel="external">Twitter</a></li>
                     <li><a class="googleplus" href="#" title="Google +" rel="external">Google+</a></li>
---
+++ impala-web/validation.php
@@ -0,0 +1,27 @@
+<?php 
+$title="Atmosphère - Validation de la commande";
+$description="Tous les événements les plus improbables. Rencontrez de nouvelles personnes, partagez vos connaissances et expériences et découvrez de nouveaux lieux.";
+
+include("includes/header.php") 
+;?>
+        <h2>Validation du paiement</h2>
+        <section id="validation-content">
+            <!-- Ici les pictos étapes : Récupérer le code de Greg -->
+            
+            <div id="message-validation">
+                <h3>Commande validée ! Projet étudiant</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam venenatis ante a arcu bibendum sagittis. Donec eget placerat leo. Donec sit amet ipsum et dolor faucibus malesuada nec lobortis diam. Cras vestibulum posuere nisl, ac cursus mi. Vivamus quis nisi quis tortor iaculis varius. Suspendisse tincidunt ut nibh et venenatis. Sed posuere mauris nulla, id elementum orci ultricies eget.</p>
+                <h4>Pour soutenir notre projet, n'hésitez pas à nous suivre :</h4>
+                <ul class="social-networks list-inline">
+                    <li><a class="facebook" href="#" title="Facebook" rel="external">Facebook</a></li>
+                    <li><a class="twitter" href="#" title="Twitter" rel="external">Twitter</a></li>
+                    <li><a class="googleplus" href="#" title="Google +" rel="external">Google+</a></li>
+                    <li><a class="pinterest" href="#" title="Pinterest" rel="external">Pinterest</a></li>
+                    <li><a class="youtube" href="#" title="Youtube" rel="external">Youtube</a></li>
+                </ul>
+                <p>Ou si vous souhaitez avoir plus d'informations sur le projet, contactez-nous à l'adresse suivante :</p>
+                <p id="mail-impala">iesa.impala@gmail.com</p>
+            </div>
+
+        </section>
+<?php include("includes/footer.php"); ?>
