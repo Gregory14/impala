d744cf25a296c8765ef79e4cccaa455fc211b043
Gregory14 <gregory.joly.14@gmail.com>
2014-06-25 23:39:29 +0200

    Add Page Panier

--- impala-web/espace-tickets.php
+++ impala-web/espace-tickets.php
@@ -4,8 +4,29 @@ $description="Une petite description du lieu";
 
 include("includes/header.php");
 ?>
-    <h2>Mon panier</h2>
-    <br>
+    <h2>Qu’est-ce qu’il y a dedans ?</h2>
+    <section id="tickets-content" >
+      <h3>Mes tickets</h3>
+        <img class="ticketThumbnail" src="#" alt="">
+      <div class="infoTicket">  
+        <div class="infoLeft inline-block"> 
+          <h4>Conférence Architecture Juin 2014</h4>
+          <p>Quantité : 3 places</p>
+        </div>
+        <div class="ticketPrice inline-block">
+          <p>15€</p>
+          <p><a href="#">supprimer</a></p>
+        </div>
+        <p>Veuillez remplir les nom(s) et prénom(s) des bénéficiaires des billets.</p>
+            <form name="ticketNames" action="#" method="post">
+                    <input class="blaze inline-block" name="subject" type="text" required tabindex="1" value="" placeholder="Blaze">
+                    <input class="smala inline-block" name="name" type="text" required tabindex="2" value="" placeholder="Nom d’smala">
+                <input class="button" name="send" type="submit" value="Passer ma commande" class="button" />
+            </form>
+      </div>
+      <div class="clear"></div>
+    </section>
+    <!-- <br>
     <br>
     <p>Mes events en cours</p>
     <?php 
@@ -23,5 +44,5 @@ include("includes/header.php");
     else{
         echo "Vous n'avez encore réservé de places";
     }
-?>
+?> -->
 <?php include("includes/footer.php"); ?>
