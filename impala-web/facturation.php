d96cab5c37b0f823f66862d045eefad41af206a9
Gregory14 <gregory.joly.14@gmail.com>
2014-06-26 02:13:19 +0200

    End Page Facturation

--- impala-web/facturation.php
+++ impala-web/facturation.php
@@ -12,34 +12,43 @@ include("includes/header.php")
                 <h3>1- Adresse de facturation</h3>
                 <form name="formBilling" action="" method="post">
                     <label for="firstname" class="inline-block">Blaze
-                        <input id="firstname" name="firstname" type="text" required tabindex="1" value="">
+                        <input id="firstname" name="firstname" type="text" required tabindex="1" value="" placeholder="Marc">
                     </label>
-                    <label for="address" class="inline-block">Nom d'smala
-                        <input id="address" name="email" type="text" required tabindex="2" value="">
+                    <label for="lastname" class="inline-block">Nom d'smala
+                        <input id="lastname" name="email" type="text" required tabindex="2" value="" placeholder="Morin">
                     </label>
-                    <label for="address">Adresse
-                        <input id="address" name="address" type="text" required tabindex="3" value="">
+                    <label for="address" class="inline-block">Adresse
+                        <input id="address" name="address" type="text" required tabindex="3" value="" placeholder="47, rue du Four">
                     </label>
                     <label for="city" class="inline-block">Gourbi
-                        <input id="city" name="city" type="text" required tabindex="4" value="">
+                        <input id="city" name="city" type="text" required tabindex="4" value="" placeholder="Paris">
                     </label>
                     <label for="postal" class="inline-block">Code postal
-                        <input id="postal" name="postal" type="text" required tabindex="5" value="">
+                        <input id="postal" name="postal" type="text" required tabindex="5" value="" placeholder="75019">
                     </label>
-                    <label for="country">Pays
-                        <input id="country" name="country" type="text" tabindex="6" required value="">
+                    <label for="country" class="inline-block">Pays
+                        <input id="country" name="country" type="text" tabindex="6" required value="" placeholder="France">
                     </label>
 
                 <h3>2- Email de livraison</h3>
                     <label for="email">Email
-                        <input id="email" name="email" type="email" required tabindex="7" value="">
+                        <input id="email" name="email" type="email" required tabindex="7" value="" placeholder="47, rue du Four">
                     </label>
                 <h3>3- Choix du paiement</h3>
-                    <input type="radio"/>
+                    <input class="inline-block" type="radio" name="paiement" value="cb"/> Carte Bleu
+                    <input class="inline-block" type="radio" name="paiement" value="cheque"> Cheque
+                    <input class="inline-block" type="radio" name="paiement" value="espece"> Espece
+
+                    <input class="button" name="pay" type="submit" value="Valider ma commande" class="button" />
                 </form>
             </div>
             <div id="order-summary" class="inline-block">
-                <h3>Récapitulatif de ma commande</h3>
+                <h3>Récapitulatif</h3>
+                <p id="sum">Total : 15€</p>
+                <p id="commanderecap">Ma commande</p>
+                <p>Conférence architecture</p>
+                <p>29 juin 2014</p>
+                <p>Quantité : <span id="nbrtotal">3 places</span></p>
             </div>
         </section>
 <?php include("includes/footer.php"); ?>
