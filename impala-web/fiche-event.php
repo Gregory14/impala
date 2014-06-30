aeb25e9526b29c1c075b43bb07f6d08e14a86c96
mbouras <marine.bouras@gmail.com>
2014-06-30 23:36:00 +0200

    MAJ CSS page fiche-lieu et fiche-event

--- impala-web/fiche-event.php
+++ impala-web/fiche-event.php
@@ -16,35 +16,35 @@ include("includes/header.php");
         echo '<strong style="color:red;">VOTRE TICKET A BIEN ETE AJOUTE A VOTRE PANIER</strong>';
     }
 ?>
-        <section class="fiche-content container">
-            <p><a href="evenements.php">Retour à tous les événéments</a></p>
-            <div class="fiche-title">
-                <h2><?php echo $donnees['title']; ?></h2>
-                <p class="tag"><?php echo $donnees['tag'] ;?></p>
-            </div>
-            <div class="informations">
+        <section class="container">
+            <p><a href="evenements.php" class="back" title="retour à tous les évènements"><</a></p>
+            <div class="fiche-content">
+                <div class="fiche-title">
+                    <h2><?php echo $donnees['title']; ?></h2>
+                    <p class="tag"><?php echo $donnees['tag'] ;?></p>
+                </div>
+                <div class="informations">
+                    <ul>
+                        <li class="address"><?php echo $donnees['address'] ;?></li>
+                        <li class="transport"><?php echo $donnees['transport'] ;?></li>
+                        <li><?php echo $donnees['price'];?>€</li>
+                        <li class="time">Horaires</li>
+                        <li class="telephone"><?php echo $donnees['telephone'] ;?></li>
+                        <li class="website"><?php echo $donnees['website'] ;?></li>
+                    </ul>
+                </div>
+                <a href="#" class="button share">Partager sur Facebook</a>
+                <div class="fiche-description">
+                    <h3>P'tite description</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consectetur mattis tincidunt. Morbi congue nisl in mollis cursus. Nunc rutrum euismod aliquam. Nulla lobortis mauris odio, non iaculis orci porttitor ut. Donec a hendrerit nisl. Nulla quis est id libero vestibulum rutrum.</p>
+                </div>
+                <div class="pictures">
+                    <img src="<?php echo $donnees['bigimage']; ?>" alt="visuel du lieu <?php echo $donnees['title']; ?>">
+                </div>
                 <ul>
-                    <li><?php echo $donnees['address'] ;?></li>
-                    <li><?php echo $donnees['transport'] ;?></li>
-                    <li><?php echo $donnees['price'];?>€</li>
-                </ul>
-                <ul>
-                    <li>Horaires</li>
-                    <li><?php echo $donnees['telephone'] ;?></li>
-                    <li><?php echo $donnees['website'] ;?></li>
+                    <li><a href="traitement/panier.php?id=<?php echo $donnees['id']; ?>" class="button">Ajouter à mes tickets</a></li>
+                    <li><a href="#" class="button">Achat rapide</a></li>
                 </ul>
             </div>
-            <a href="#" class="button share">Partager sur Facebook</a>
-            <div class="fiche-description">
-                <h3>P'tite description</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consectetur mattis tincidunt. Morbi congue nisl in mollis cursus. Nunc rutrum euismod aliquam. Nulla lobortis mauris odio, non iaculis orci porttitor ut. Donec a hendrerit nisl. Nulla quis est id libero vestibulum rutrum.</p>
-            </div>
-            <div class="pictures">
-                <p>Ici le slider</p>
-            </div>
-            <ul>
-                <li><a href="traitement/panier.php?id=<?php echo $donnees['id']; ?>" class="button">Ajouter à mes tickets</a></li>
-                <li><a href="#" class="button">Achat rapide</a></li>
-            </ul>
         </section>
 <?php include("includes/footer.php"); ?>
